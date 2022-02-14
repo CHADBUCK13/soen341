802a9252423593383e880276f795f8105190ca61
CHADBUCK13 <chadbuck13@gmail.com>
2022-02-14 10:53:24 -0500

    Test scenario that should pass tests

--- .venv/project/ecommerce/tests/tests.py
+++ .venv/project/ecommerce/tests/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def speak(self):
+        return f"The {self.name} says \"{self.sound}\""
+
+class test_animals(TestCase):        
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        lion = Animal("lion","roar")
+        self.assertEqual(lion.speak(), 'The lion says "roar"')
+    
+    # def test_animals_cant_speak(self):
+    #     """Animals that cant speak are correctly identified"""
+    #     lion = Animal("lion","")
+    #     self.assertEqual(lion.speak(), 'The lion says "roar"')
