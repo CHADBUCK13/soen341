3abc5a406f490eeb99179196db27e9f3eb2464d9
tommymorrisson <98559624+tommymorrisson@users.noreply.github.com>
2022-04-05 22:51:08 -0400

    fixed

--- .venv/project/ecommerce/api/shopping_cart.py
+++ .venv/project/ecommerce/api/shopping_cart.py
@@ -16,7 +16,8 @@ def add_item_to_cart(email, item_id, quantity):
     """
     adding items to cart
     """
-    #collection shopping_cart contains subcollections
+    #collection shopping_cart contains subcollections of emails that each contain a a document itemId
+    
     try:
         cart_ref = db.collection('shopping_cart').document(email)
         if cart_ref.get().exists:
@@ -28,62 +29,61 @@ def add_item_to_cart(email, item_id, quantity):
             cart_ref.set(cart_data)
 
         return True
-
+ 
 
     except HTTPError as error:
-        return json.loads(error.strerror)
+            return json.loads(error.strerror)
 
 
 def get_items_from_cart(email):
     """
-    from email find items in item database with same itemID, return collection of items with info
+    from email find items in item database with same itemID, return collection of items with all information
     """
-    shopping_cart_ref = db.collection('shopping_cart').document(email).get()
 
+    shopping_cart_ref = db.collection('shopping_cart').document(email).get()
     try:
+        
         return shopping_cart_to_dict(shopping_cart_ref)
 
     except HTTPError as error:
-        return json.loads(error.strerror)
+            return json.loads(error.strerror)
 
 
 def shopping_cart_to_dict(cart_document):
-    """
-    save shopping cart collection as a dictionnary
-    """
     all_items = []
+  
     cart=cart_document.to_dict()['items']
 
 
-    for item_id in cart:
+    for itemID in cart:
         item = get_item_by_id(item_id)
         quantity = cart[item_id]
         all_items.append([item,quantity])
 
     return all_items
 
+
+    
 def delete_items_from_cart(email, item_id):
     """
-    delete item
+    delete item 
     """
     try:
         shopping_cart_ref = db.collection('shopping_cart').document(email)
         shopping_cart_ref.get()
         shopping_cart_ref.update({'items.'+item_id: fs.DELETE_FIELD})
-
+        
         return True
-
+ 
     except HTTPError as error:
-        return json.loads(error.strerror)
+            return json.loads(error.strerror)
 
 
 
 def update_item_quantity(email, item_id, quantity):
-    """
-    update the quantity of an item
-    """
-    if quantity<0:
-        return False
+
+    #if quantity<0:
+    #    return False
 
     try:
         item_ref = db.collection('shopping_cart').document(email)
@@ -94,16 +94,17 @@ def update_item_quantity(email, item_id, quantity):
 
 
 def create_cart(email):
-    """
-    create a new shopping cart
-    """
+
     try:
         cart_ref = db.collection('shopping_cart').document(email)
         if cart_ref.get().exists:
             return False
 
-        cart_ref.set({'items':{}})
+        else:
+            cart_ref.set({'items':{}})
 
         return True
+ 
+
     except HTTPError as error:
-        return json.loads(error.strerror)
+            return json.loads(error.strerror)
