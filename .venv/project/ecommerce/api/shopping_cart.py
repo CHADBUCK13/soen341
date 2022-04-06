d6792f0e137c22db6f5b86a14535b2c9b16a0eb4
tommymorrisson <98559624+tommymorrisson@users.noreply.github.com>
2022-04-05 22:55:35 -0400

    fixed again

--- .venv/project/ecommerce/api/shopping_cart.py
+++ .venv/project/ecommerce/api/shopping_cart.py
@@ -46,16 +46,19 @@ def get_items_from_cart(email):
         return shopping_cart_to_dict(shopping_cart_ref)
 
     except HTTPError as error:
-            return json.loads(error.strerror)
+        return json.loads(error.strerror)
 
 
 def shopping_cart_to_dict(cart_document):
+    """
+    save shopping cart collection as a dictionnary
+    """
     all_items = []
   
     cart=cart_document.to_dict()['items']
 
 
-    for itemID in cart:
+    for item_id in cart:
         item = get_item_by_id(item_id)
         quantity = cart[item_id]
         all_items.append([item,quantity])
@@ -66,22 +69,24 @@ def shopping_cart_to_dict(cart_document):
     
 def delete_items_from_cart(email, item_id):
     """
-    delete item 
+    delete item
     """
     try:
         shopping_cart_ref = db.collection('shopping_cart').document(email)
         shopping_cart_ref.get()
         shopping_cart_ref.update({'items.'+item_id: fs.DELETE_FIELD})
-        
+
         return True
- 
+
     except HTTPError as error:
-            return json.loads(error.strerror)
+        return json.loads(error.strerror)
 
 
 
 def update_item_quantity(email, item_id, quantity):
-
+    """
+    update the quantity of an item
+    """
     #if quantity<0:
     #    return False
 
@@ -90,11 +95,13 @@ def update_item_quantity(email, item_id, quantity):
         item_ref.update({'items.'+item_id: quantity})
 
     except HTTPError as error:
-            return json.loads(error.strerror)
+        return json.loads(error.strerror)
 
 
 def create_cart(email):
-
+    """
+    create a new shopping cart
+    """
     try:
         cart_ref = db.collection('shopping_cart').document(email)
         if cart_ref.get().exists:
@@ -104,7 +111,7 @@ def create_cart(email):
             cart_ref.set({'items':{}})
 
         return True
- 
+
 
     except HTTPError as error:
-            return json.loads(error.strerror)
+        return json.loads(error.strerror)
